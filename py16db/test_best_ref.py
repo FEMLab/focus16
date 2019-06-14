5c19d1941e2c953e66f2c5d89532c94bf2fba4c5
Ben Nolan <alexandranolan@samsmacbookpro.nuigalway.ie>
2019-06-14 12:18:55 +0100

    add test for plentyofbugs

--- py16db/test_best_ref.py
+++ py16db/test_best_ref.py
@@ -1,8 +1,16 @@
-from best_ref import mash
+from run_all import pob
+import os
+import shutil
 
-def test_mash():
-    test_result = mash(wd="", readfile="", organism="", strains="", experiment="", output="")
+def test_pob():
+    plasmids = "./plentyofbugs/test_data/plasmids/" 
+    reads = "./plentyofbugs/test_data/test_reads1.fq" 
+    output_dir="./pob_test_result/"
+    if os.path.exists(output_dir):
+        shutil.rmtree(output_dir)
+    test_result = pob(genomes_dir=plasmids, readsf=reads, output_dir=output_dir)
     print(test_result)
+    assert ["./plentyofbugs/test_data/plasmids/NC_009837.1.fasta", "0.0340249"] == test_result
     return()
 
     
