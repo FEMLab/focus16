b23bc911645aaba41c48cb866ea935755547fb42
Ben Nolan <alexandranolan@samsmacbookpro.nuigalway.ie>
2019-06-19 16:07:59 +0100

    Improved test for coverage function added downsample function

--- py16db/test_coverage.py
+++ py16db/test_coverage.py
@@ -1,11 +1,23 @@
-from run_all import coverage
+from run_all import get_coverage, downsample
 import os
 
+
 def test_coverage():
+#genome is built from NC_0176759.1 ~150000bp
     reads = "./plentyofbugs/test_data/test_reads1.fq"
     destination = "../test/0/"
-    test_result = coverage(approx_length=2000000, fastq1=reads, SRA="SRR8443698", destination=destination, N=50)
+    test_result = get_coverage(approx_length=150000, fastq1=reads)
     print(test_result)
-    assert 0.735  == test_result
+    assert 9.8  == test_result
+    return()
+
+def test_downsample():
+#genome is built from NC_0176759.1 ~150000bp
+    reads = "./plentyofbugs/test_data/test_reads1.fq"
+    destination = "../test/0/"
+    reads1, reads2 = downsample(approx_length=150000, fastq1=reads, fastq2=reads,
+                             destination=destination, maxcoverage=2)
+    down_cov = get_coverage(approx_length=150000, fastq1=test_result)
+    assert 1.993 == down_cov
     return()
     
