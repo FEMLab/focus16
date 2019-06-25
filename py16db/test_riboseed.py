40757ae9652d556347f56161d6dfd0bff30d6363
Ben Nolan <alexandranolan@samsmacbookpro.nuigalway.ie>
2019-06-25 16:42:35 +0100

    Add example_data/reads*.fq as input reads

--- py16db/test_riboseed.py
+++ py16db/test_riboseed.py
@@ -1,20 +1,34 @@
 from run_all import run_riboseed
 import os
 import shutil
+from nose.tools.nontrivial import with_setup
+
+
+def setup_func():
+    "set up test fixtures"
+    if os.path.exists("./py16db/riboSeed"):
+        shutil.rmtree("./py16db/riboSeed/")
+    pass
+
+def teardown_func():
+    "tear down test fixtures"
+    shutil.rmtree("./py16db/riboSeed/")
+
+
+@with_setup(setup_func, teardown_func)
 
 def test_riboseed():
-    downreadsf="./data/test_data/mutans/downsampled/downsampledreadsf.fastq"
-    downreadsr="./data/test_data/mutans/downsampled/downsampledreadsr.fastq"
-    output_dir="./data/test_data/mutans/riboSeed/"  
-    sra="./data/test_data/mutans/NC_004350.2.fna"
-    if os.path.isdir("./data/test_data/mutans/riboSeed/"):
-        shutil.rmtree("./data/test_data/mutans/riboSeed/")
-        
-    run_riboseed(sra=sra, readsf=downreadsf,
-                 readsr=downreadsr, cores="4", threads="1",
-                 v="1", output=output_dir)
+    readsf="./example_data/reads1.fq"
+    readsr="./example_data/reads2.fq"
+    output_dir="./py16db/riboSeed/"
+    os.makedir=(output_dir)
+    sra="./example_data/NC_013928.1.fna"
+          
+    run_riboseed(sra=sra, readsf=readsf,
+                 readsr=readsr, cores="4", threads="1",
+                 output=output_dir)
     
     
-    assert os.path.exists("./data/test_data/mutans/riboSeed/seed/final_long_reads/")
+    assert os.path.exists("./py16db/riboSeed/seed/final_long_reads/")
 
     return()
