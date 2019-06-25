2a0b748e342fe719d3fbccf0f581bf7e9bc306e3
Ben Nolan <alexandranolan@samsmacbookpro.nuigalway.ie>
2019-06-25 16:43:38 +0100

    Add example_data/reads*.fq as input reads

--- py16db/test_coverage.py
+++ py16db/test_coverage.py
@@ -18,6 +18,7 @@ def setup_func():
         shutil.rmtree("./py16db/downsample_test_result/")
     pass
 
+
 def teardown_func():
     "tear down test fixtures"
     shutil.rmtree("./py16db/downsample_test_result/")
@@ -27,6 +28,7 @@ def test_downsample():
 #genome is built from NC_0176759.1 ~150000bp
     reads = os.path.join(os.path.dirname(__file__), "data", "test_data", "test_reads1.fq")
     os.makedirs("./py16db/downsample_test_result/")
+    os.makedirs("./py16db/downsample_test_result/downsampled/")
     destination = "./py16db/downsample_test_result/"
     reads1, reads2 = downsample(approx_length=150000, fastq1=reads, fastq2=reads,
                                 destination=destination, maxcoverage=2)
