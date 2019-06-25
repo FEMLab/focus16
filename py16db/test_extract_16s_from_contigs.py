b871dc52182cecbfaa0c2019b49a9a37f18b300e
Ben Nolan <alexandranolan@samsmacbookpro.nuigalway.ie>
2019-06-25 16:41:46 +0100

    [WIP] Add NC_018089.1.fna as test

--- py16db/test_extract_16s_from_contigs.py
+++ py16db/test_extract_16s_from_contigs.py
@@ -1,9 +1,24 @@
 from run_all import extract_16s_from_contigs
+from nose.tools.nontrivial import with_setup
+import os
+import shutil
 
+#def setup_func():
+#    "set up test fixtures"
+#    if os.path.exists("./py16db/extract_test/"):
+#       shutil.rmtree("./py16db/extract_test/")
+#    pass
+#
+#def teardown_func():
+#    "tear down test fixtures"
+#    shutil.rmtree("./py16db/extract_test/")
+#
+#@with_setup(setup_func, teardown_func)
 def test_extract():
-    input_contigs="./data/test_data/mutans/downsampled/downsampledreadsf.fastq"
-    barr_out="../test/0/riboSeed/barrnap"
-    output="../test/0/riboSeed/ribo16s/"
+    os.makedirs("./py16db/extract_test/")
+    input_contigs = os.path.join(os.path.dirname(__file__),  "data", "test_data", "riboseed", "genomes", "NC_018089.1.fna")
+    barr_out="./py16db/extract_test/barrnap"
+    output="./py16db/extract_test/ribo16"
     test_result=extract_16s_from_contigs(input_contigs=input_contigs,
                                          barr_out=barr_out, output=output)
     assert []  == test_result
