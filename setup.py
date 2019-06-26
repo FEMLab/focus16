4a1d6fec5b8e9854025a6f893cd02e9237e55619
Ben Nolan <alexandranolan@samsmacbookpro.nuigalway.ie>
2019-06-26 12:09:02 +0100

    Fix line 24 SyntaxError

--- setup.py
+++ setup.py
@@ -15,30 +15,30 @@ setup(name='16db',
                'py16db/test_best_ref.py',
                'py16db/test_coverage.py',
                'py16db/test.py',
-               'get_n_genomes.py'],
+               'py16db/get_n_genomes.py',
+               'py16db/fetch_sraFind_data.py'],
       install_requires = ["biopython"],
       zip_safe=False,
       include_package_data=True,
       package_data={
-          'py16db':[
-              'example_data':
-                  ['example_data/NC_013928.1.fna',
-                   'example_data/reads1.fq.gz',
-                   'example_data/reads2.fq.gz',
-                   'example_data/README.md',
-                   ],
-              'test_data':
-                  ['plasmids/*',
-                   'test_reads1.fq',
-                   'sraFind-Contig-biosample-with-SRA-hits.txt',
-                   'NC_017659.1.fasta',
-                   ],
-              },
-          entry_points={ 
-              'console_scripts': [
-                  '16db = py16db.run_all.py:main',
-                  ],
-              }
-          )
+        'example_data':
+            ['example_data/NC_013928.1.fna',
+             'example_data/reads1.fq.gz',
+             'example_data/reads2.fq.gz',
+             'example_data/README.md',
+             ],
+        'test_data':
+            ['plasmids/*',
+             'test_reads1.fq',
+             'sraFind-Contig-biosample-with-SRA-hits.txt',
+             'NC_017659.1.fasta',
+             ],
+        },
+      entry_points={ 
+        'console_scripts': [
+            '16db = py16db.run_all.py:main',
+            ],
+        }
+      )
                     
           
