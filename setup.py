1703b79e14f30e65a320b45bb2ad35dbe677226e
Ben Nolan <alexandranolan@samsmacbookpro.nuigalway.ie>
2019-06-26 11:51:00 +0100

    Add scripts, package_data
    Former-commit-id: 99dec097dbc082808c3f700dd7e654171196fc7d

--- setup.py
+++ setup.py
@@ -1,15 +1,44 @@
 from setuptools import setup
 
 setup(name='16db',
-      version='0.1',
+      version='0.1.0',
       description='Draft genome reassembly using riboSeed, for the construction ' + 
-      'of new databases',
+      'of new 16S databases',
       url='http://github.com/BenNolann/16db',
       author='Ben Nolan',
       author_email='N.BEN1@nuigalway.ie',
       license='MIT',
+      keywords='bioinformatics, assembly, 16s, database',
       packages=['py16db'],
-      scripts=['py16db/get_sra_for_organism.py', 'best_ref.py', 
-               'get_n_genomes.py', 'riboseed.py'],
-      install_requires = ["biopython"]
-      zip_safe=False)
+      scripts=['py16db/run_all.py',
+               'py16db/test_ave_read_len.py',
+               'py16db/test_best_ref.py',
+               'py16db/test_coverage.py',
+               'py16db/test.py',
+               'get_n_genomes.py'],
+      install_requires = ["biopython"],
+      zip_safe=False),
+      include_package_data=True,
+      package_data={
+          'py16db':[
+              'example_data':
+                  ['example_data/NC_013928.1.fna',
+                   'example_data/reads1.fq.gz',
+                   'example_data/reads2.fq.gz',
+                   'example_data/README.md',
+                   ],
+              'test_data':
+                  ['plasmids/*',
+                   'test_reads1.fq',
+                   'sraFind-Contig-biosample-with-SRA-hits.txt',
+                   'NC_017659.1.fasta',
+                   ],
+              },
+          entry_points={ 
+              'console_scripts': [
+                  '16db = py16db.run_all.py:main',
+                  ],
+              }
+          )
+                    
+          
