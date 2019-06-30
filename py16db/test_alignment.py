fa21fecbceb39483864921b53323e7bb75d35d13
BenNolann <n.ben1@nuigalway.ie>
2019-06-30 17:53:01 +0100

    Fix path for test_alignment.py
    Former-commit-id: d9d8f60ac533a4a45daadd062533e7d17f530c2e

--- py16db/test_alignment.py
+++ py16db/test_alignment.py
@@ -2,22 +2,32 @@ from .run_all import alignment
 from nose.tools.nontrivial import with_setup
 import os
 import shutil
+import unittest
 
-def setup_func():
-   "set up test fixtures"
-   if os.path.exists("./py16db/alignment_test/"):
-       shutil.rmtree("./py16db/alignment_test/")
-       pass
-
-def teardown_func():
-    "tear down test fixtures"
-    shutil.rmtree("./py16db/alignment_test/")
-
-@with_setup(setup_func, teardown_func)
-def test_alignment():
-    test_output = "./py16db/alignment_test/"
-    os.makedirs(test_output)
-    test_fasta = "./py16db/test_data/test_16s_multiline.fasta"
-    test_result = alignment(fasta=test_fasta, output=test_output)
-    print(test_result)
-    assert os.path.exists("./py16db/alignment_test/alignment/MSA.fasta")  
+class alignmentTest(unittest.TestCase):
+   """ test for alignment function
+   """
+   def setUp(self):
+      self.test_dir = os.path.join(os.path.dirname(__file__),
+                                   "alignment_test")
+      self.fasta = os.path.join(os.path.dirname(__file__),
+                                "test_data", "test_16s_multiline.fasta")
+      if os.path.exists(self.test_dir):
+         shutil.rmtree(self.test_dir)
+         
+   #def tearDown(self):
+   #   """ tear down test fixtures
+   #   """
+   #   shutil.rmtree(self.test_dir)
+   
+   def test_alignment(self):
+      test_output = (self.test_dir)
+      os.makedirs(test_output)
+      test_fasta = (self.fasta)
+      test_result = alignment(fasta=test_fasta, output=test_output)
+      print(test_result)
+      mafftoutput = os.path.join(self.test_dir, "alignment", "MSA.fasta")
+      with open(mafftoutput, "r") as infile:
+         firstline = infile.readline().strip()
+         print(firstline)
+      assert firstline == ">CP003686.1 :16889:18441"
