4829ecfc96216e066757d51e2a06ddb4ad44e469
BenNolann <n.ben1@nuigalway.ie>
2019-07-01 16:51:19 +0100

    Fix paths for run_sickle test
    Former-commit-id: 578f99518685140d7fe7b6b38516b7887ea4afb2

--- py16db/test_run_sickle.py
+++ py16db/test_run_sickle.py
@@ -5,34 +5,35 @@ import unittest
 from Bio import SeqIO
 from nose.tools.nontrivial import with_setup
 
-
 def file_len(fname):
     with open(fname) as f:
         for i, l in enumerate(f):
             pass
-    return i + 1
+        return i + 1
 
-def setup_func():
-    "set up test fixtures"
-    if os.path.exists("./py16db/testsickle/"):
-        shutil.rmtree("./py16db/testsickle/")
-    pass
+class test_run_sickle(unittest.TestCase):
+    '''test for the run sickle function
+    '''
+    def setUp(self):
+        self.test_dir = os.path.join(os.path.dirname(__file__), "testsickle", "")
+        self.fastq1 = os.path.join(os.path.dirname(__file__), "test_data", "mutans",
+                                   "downsampled", "downsampledreadsf.fastq")
+        if os.path.exists(self.test_dir):
+            shutil.rmtree(self.test_dir)
+            
+    def teardown(self):
+        "tear down test fixtures"
+        shutil.rmtree(self.test_dir)
 
-def teardown_func():
-    "tear down test fixtures"
-    shutil.rmtree("./py16db/testsickle/")
-
-
-@with_setup(setup_func, teardown_func)
-@unittest.skipIf("TRAVIS" in os.environ and os.environ["TRAVIS"] == "true",
-                 "skipping this test on travis.CI")
-def test_sickle_singles():
-    sickle_test_dir = "./py16db/testsickle/"
-    fastq1 = os.path.join(os.path.dirname(__file__), "test_data", "mutans", "downsampled", "downsampledreadsf.fastq")
-    fastq2 = None
-    if os.path.exists(sickle_test_dir):
-        shutil.rmtree(sickle_test_dir)
-    new_fastq1, new_fastq2 = run_sickle(fastq1=fastq1, fastq2 = None,
-                                        output_dir=sickle_test_dir)
-    assert new_fastq2 == None
-    assert file_len(fastq1) > file_len(new_fastq1)
+   
+    @unittest.skipIf("TRAVIS" in os.environ and os.environ["TRAVIS"] == "true",
+                     "skipping this test on travis.CI")
+    def test_sickle_singles(self):
+        sickle_test_dir = (self.test_dir)
+        fastq1 = (self.fastq1)
+        fastq2 = None
+        new_fastq1, new_fastq2 = run_sickle(fastq1=self.fastq1, fastq2 = None,
+                                                output_dir=self.test_dir)
+        assert new_fastq2 == None
+        assert file_len(fastq1) > file_len(new_fastq1)
+            
