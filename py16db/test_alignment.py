10696519b68d6e7b45b761b531ba2c2a52b6291d
BenNolann <n.ben1@nuigalway.ie>
2019-07-10 16:50:28 +0100

    Add long tests for post-riboseed functions
    Former-commit-id: 4ca6778ddb47048cb3472aa92ffd6738234fe424

--- py16db/test_alignment.py
+++ py16db/test_alignment.py
@@ -5,32 +5,59 @@ import shutil
 import unittest
 import logging as logger
 
-class alignmentTest(unittest.TestCase):
-   """ test for alignment function
+# class alignmentTest(unittest.TestCase):
+#    """ test for alignment function
+#    """
+#    def setUp(self):
+#       self.test_dir = os.path.join(os.path.dirname(__file__),
+#                                    "alignment_test")
+#       self.fasta = os.path.join(os.path.dirname(__file__),
+#                                 "test_data", "test_16s_multiline.fasta")
+#       if os.path.exists(self.test_dir):
+#          shutil.rmtree(self.test_dir)
+         
+#    def tearDown(self):
+#       """ tear down test fixtures
+#       """
+#       shutil.rmtree(self.test_dir)
+
+#    @unittest.skipIf("TRAVIS" in os.environ and os.environ["TRAVIS"] == "true",
+#                     "skipping this test on travis.CI")    
+#    def test_alignment(self):
+#       test_output = (self.test_dir)
+#       os.makedirs(test_output)
+#       test_fasta = (self.fasta)
+#       test_result = alignment(fasta=test_fasta, output=test_output, logger=logger)
+#       print(test_result)
+#       mafftoutput = os.path.join(self.test_dir, "alignment", "MSA.fasta")
+#       with open(mafftoutput, "r") as infile:
+#          firstline = infile.readline().strip()
+#          print(firstline)
+#       assert firstline == ">CP003686.1 :16889:18441"
+
+
+
+class alignmentLongTest(unittest.TestCase):
+   """ test for the alignment step, using 5 whole genome sequences from E. coli
    """
    def setUp(self):
-      self.test_dir = os.path.join(os.path.dirname(__file__),
-                                   "alignment_test")
-      self.fasta = os.path.join(os.path.dirname(__file__),
-                                "test_data", "test_16s_multiline.fasta")
-      if os.path.exists(self.test_dir):
-         shutil.rmtree(self.test_dir)
+      self.test2_dir = os.path.join(os.path.dirname(__file__),
+                                    "alignment_test_long")
+      self.inputfasta = os.path.join(os.path.dirname(__file__), "test_data",
+                                     "ecoli16s", "ribo16")
+      if os.path.exists(self.test2_dir):
+         shutil.rmtree(self.test2_dir)
          
-   def tearDown(self):
-      """ tear down test fixtures
-      """
-      shutil.rmtree(self.test_dir)
-
+    def tearDown(self):
+       shutil.rmtree(self.test2_dir)
+   
+   
    @unittest.skipIf("TRAVIS" in os.environ and os.environ["TRAVIS"] == "true",
-                    "skipping this test on travis.CI")    
-   def test_alignment(self):
-      test_output = (self.test_dir)
-      os.makedirs(test_output)
-      test_fasta = (self.fasta)
-      test_result = alignment(fasta=test_fasta, output=test_output, logger=logger)
-      print(test_result)
-      mafftoutput = os.path.join(self.test_dir, "alignment", "MSA.fasta")
-      with open(mafftoutput, "r") as infile:
-         firstline = infile.readline().strip()
-         print(firstline)
-      assert firstline == ">CP003686.1 :16889:18441"
+                       "skipping this test on travis.CI")       
+   def test_alignmentlong(self):
+      test_output = (self.test2_dir)
+      inputfasta = (self.inputfasta)
+      test_result = alignment(fasta=inputfasta, output=test_output, logger=logger)
+    
+
+      assert os.path.exists(test_result)
--- py16db/test_extract_16s_from_contigs.py
+++ py16db/test_extract_16s_from_contigs.py
@@ -9,7 +9,7 @@ class extractTest(unittest.TestCase):
    """ test for extract_16s_from_contigs 
    """
    def setUp(self):
-      self.test_dir = os.path.join(os.path.dirname(__file__),
+      self.test_dir = os.path.join(os.path.dirname(__file__),                     
                                    "extract_test")
       self.contigs = os.path.join(os.path.dirname(__file__),
                                   "test_data", "riboseed", "genomes", "NC_018089.1.fna")
@@ -34,8 +34,39 @@ class extractTest(unittest.TestCase):
                                            barr_out=barr_out, output=output,
                                            logger=logger)
       assert os.path.exists(test_result)
-            
+       
 
+class extractTestLong(unittest.TestCase):
+   """ test for extract_16s_from_contigs 
+   """
+   def setUp(self):
+      self.test_dir = os.path.join(os.path.dirname(__file__),                     
+                                   "extract_test")
+      self.contigs = os.path.join(os.path.dirname(__file__),
+                                  "test_data", "ecoligenomes", "")
+      self.barrnap = os.path.join(self.test_dir, "barrnap")
+      self.out_dir = os.path.join(self.test_dir, "ribo16")
+      if os.path.exists(self.test_dir):
+         shutil.rmtree(self.test_dir)
+      
+   def tearDown(self):
+      """tear down test fixtures
+      """
+      shutil.rmtree(self.test_dir)
 
-    
+   @unittest.skipIf("TRAVIS" in os.environ and os.environ["TRAVIS"] == "true",
+                    "skipping this test on travis.CI")
+   
+   def test_extract(self):
+      os.makedirs(self.test_dir)
+      barr_out = (self.barrnap)
+      output = (self.out_dir)
+
+      for file in os.listdir(self.contigs):
+         contig = os.path.join(self.contigs, file)
+         test_result = extract_16s_from_contigs(input_contigs=contig,
+                                                barr_out=barr_out, output=output,
+                                                logger=logger)
+         assert os.path.exists(output)
+
 
