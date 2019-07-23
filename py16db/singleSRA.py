679e903fa15f37ceae0bdd28da5b6dd4cd5e8ef2
BenNolann <n.ben1@nuigalway.ie>
2019-07-23 10:40:54 +0100

    Returns print after loop

--- py16db/singleSRA.py
+++ py16db/singleSRA.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python                                                                                                                                                                
-
 import sys
 import os
 import collections
-import pickle
+
 
 def singleSRA():
     org_dict = {}
@@ -12,15 +11,13 @@ def singleSRA():
         for i,line in enumerate(infile):
             split_line = [x.replace('"', '').replace("'", "") for x in line.strip().split("\t")]
             gs = " ".join(split_line[11].split(" ")[0:2])
-            if gs in org_dict.keys():
-                org_dict[gs] += 1
-            else:
-                org_dict[gs] = 1
             if split_line[8].startswith("ILLUMINA"):
-                singleSRA = []
-                singleSRA.append(split_line[11]) #organism name
-                print(org_dict)
-        return()                    
+                if gs in org_dict.keys():
+                    org_dict[gs] += 1
+                else:
+                    org_dict[gs] = 1
+    print(org_dict)
+    return()                    
                       
          
 def main():
