b2410dc0fd8ecb4f41056b91dd43f404fdf51842
BenNolann <n.ben1@nuigalway.ie>
2019-07-23 10:42:08 +0100

    Add version

--- setup.py
+++ setup.py
@@ -2,11 +2,21 @@ from setuptools import setup
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
+    
+VERSIONFILE = "py16db/_version.py"
+verstrline = open(VERSIONFILE, "rt").read()
+VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
+mo = re.search(VSRE, verstrline, re.M)
+if mo:
+    verstr = mo.group(1)
+else:
+    raise RuntimeError("Unable to find version string in %s." % (VERSIONFILE,))
+
 
 setup(name='16db',
-      version='0.1.0',
       description='Draft genome reassembly using riboSeed, for the construction ' + 
       'of high resolution 16S databases',
+      version=verstr,
       url='http://github.com/BenNolann/16db',
       author='Ben Nolan',
       author_email='N.BEN1@nuigalway.ie',
