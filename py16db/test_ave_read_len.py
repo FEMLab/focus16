7c6189fcf2839bad06094b0909795f14ad85f7d9
BenNolann <n.ben1@nuigalway.ie>
2019-07-29 17:20:13 +0100

    Add altered data paths for generator.py produced data

--- py16db/test_ave_read_len.py
+++ py16db/test_ave_read_len.py
@@ -5,7 +5,7 @@ import logging as logger
 
 def test_get_ave():
     reads = os.path.join(os.path.dirname(__file__), "test_data", "test_reads1.fq")
-    test_result = get_and_check_ave_read_len_from_fastq(fastq1=reads, N=50, logger=logger)
+    test_result = get_and_check_ave_read_len_from_fastq(fastq1=reads, logger=logger)
     print(test_result)
     assert 150.0 == test_result
     return()
